5fb16effd9a76d47a1254c4604d8e416f0e14a32
lss <404629780@qq.com>
2019-05-08 16:31:03 +0800

    添加ResController

---
+++ src/main/java/com/example/gitdemo/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.gitdemo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description 用户模块
+ * @Author lss0555
+ * @Date 2019/5/8/008 16:30
+ **/
+@RestController
+public class UserController {
+
+}
